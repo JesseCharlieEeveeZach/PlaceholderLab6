91c9b4b4f3b4a70eae48b5ea86a5bc3a65115a43
JesseSound <jesseearlemccormack@gmail.com>
2023-11-07 03:53:30 -0500

    Added Town Triggers
    had a stroke of genius way too late on how to simplify the workflow... oh well

--- Assets/Scripts/TownTriggers.cs
+++ Assets/Scripts/TownTriggers.cs
@@ -24,18 +24,55 @@ public class TownTriggers : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("TownTrigger"))
+        if (other.CompareTag("TheHub"))
         {
             Debug.Log(gameObject.name + " is triggered by " + other.gameObject.name);
             townName.text = "The Hub";
             canvasVisibility.SetActive(true);
         }
+        if (other.CompareTag("Trafficka"))
+        {
+            Debug.Log(gameObject.name + " is triggered by " + other.gameObject.name);
+            townName.text = "Trafficka Cult Area";
+            canvasVisibility.SetActive(true);
+        }
+        if (other.CompareTag("totem"))
+        {
+            Debug.Log(gameObject.name + " is triggered by " + other.gameObject.name);
+            townName.text = "....Totem....";
+            canvasVisibility.SetActive(true);
+        }
+        if (other.CompareTag("ZakkariaVille"))
+        {
+            Debug.Log(gameObject.name + " is triggered by " + other.gameObject.name);
+            townName.text = "ZakkariaVille";
+            canvasVisibility.SetActive(true);
+            StartCoroutine(HideCanvasAfterDelay(3.0f));
+        }
+        if (other.CompareTag("Eevee"))
+        {
+            Debug.Log(gameObject.name + " is triggered by " + other.gameObject.name);
+            townName.text = "Eeveeville";
+            canvasVisibility.SetActive(true);
+            StartCoroutine(HideCanvasAfterDelay(3.0f));
+        }
     }
     private void OnTriggerExit(Collider other)
     {
         townName.text = "";
-        canvasVisibility.SetActive(false);
+        if (canvasVisibility.activeInHierarchy)
+        {
+            canvasVisibility.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Canvas hidden already!");
+        }
     }
 
-
+    private IEnumerator HideCanvasAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        canvasVisibility.SetActive(false);
+    }
 }
