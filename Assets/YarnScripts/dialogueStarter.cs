bd2bdbc4dbff016f70c54396b6e5cd7aecbda38f
JesseSound <61964089+JesseSound@users.noreply.github.com>
2023-11-09 17:51:09 -0500

    Adding Yarnspinner
    currently works BUT does not allow for:
    
    Options :(
    
    Still need to add a way to allow the player to move after, as well.

---
+++ Assets/YarnScripts/dialogueStarter.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class dialogueStarter : MonoBehaviour
+{
+    public string nodeName; // Specify the node name you want to start from in the Inspector.
+
+    // Reference to the Dialogue Runner component in your scene.
+    
+    public DialogueRunner dialogueRunner;
+    public LineView lineView;
+    public GameObject player;
+
+    private bool conversationStarted = false;
+
+    private void Start()
+    {
+        // Ensure the Dialogue Runner is properly set in the Inspector.
+        if (dialogueRunner == null)
+        {
+            Debug.LogError("Dialogue Runner is not assigned.");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !conversationStarted)
+        {
+            
+            player.GetComponent<FirstPersonController>().playerCanMove = false;
+            player.GetComponent<FirstPersonController>().enableHeadBob = false;
+            player.GetComponent<FirstPersonController>().enableJump = false;
+            // Start the dialogue with the specified node name.
+            dialogueRunner.StartDialogue(nodeName);
+            conversationStarted = true; // Prevent starting the conversation repeatedly.
+        }
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            lineView.UserRequestedViewAdvancement();
+            
+        }
+    }
+
+}
